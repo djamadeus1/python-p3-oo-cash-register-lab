75671d1fe9d26af3dd1666fa9bceedaf456f3a85
Ronald Roberts <djamadeus1@gmail.com>
2024-11-21 16:46:36 -0500

    cash register lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 
-class CashRegister:
-  pass
+      # Creating a virtual Cassh register
+class CashRegister:         
+    def __init__(self, discount=0):   # Uing Dunder method int to set attributes to our Cash register class
+        self.total = 0
+        self.discount = discount
+        self.items = []
+        self.last_transaction = 0
+
+    def add_item(self, name, price, quantity=1):
+            self.total += price * quantity 
+            self.last_transaction = price * quantity
+            self.items.extend([name] * quantity)
+
+    def apply_discount(self):
+        if self.discount > 0:
+            discount_amount = self.total * (self.discount / 100)
+            self.total -= discount_amount
+            print(f"After the discount, the total comes to ${self.total:.0f}.")
+        else:
+              print("There is no discount to apply.")
+
+        # if not isinstance(self.discount, (int,float)):
+        #      raise TypeError("Must be a number")
+       
+    def void_last_transaction(self):
+           self.total -= self.last_transaction
+
+          
+
+
+           
+                   
+
+    
